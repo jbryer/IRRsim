3d1713b813bd4032a5ce3e602cb18f30f36d595c
Added header for item results <JBryer@excelsior.edu>
2018-07-16 13:04:30 -0400

    Added MaxResponseDiff

--- R/as.data.frame.IRRsim.R
+++ R/as.data.frame.IRRsim.R
@@ -8,7 +8,7 @@
 as.data.frame.IRRsim <- function(x, ...) {
 	tmp <- sapply(x, FUN = function(y) {
 		unlist(y[c('nLevels', 'nEvents', 'k', 'simAgreement', 'agreement',
-				   'skewness', 'kurtosis',
+				   'skewness', 'kurtosis', 'MaxResponseDiff',
 				   'ICC1', 'ICC2', 'ICC3', 'ICC1k', 'ICC2k', 'ICC3k',
 				   'Fleiss_Kappa', 'Cohen_Kappa')])
 	}, simplify = "matrix")
