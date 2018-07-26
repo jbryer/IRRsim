fbf56cd28c582207a26b778cac9e79d88c0fddfe
Jason Bryer <jason@bryer.org>
2018-07-26 11:18:33 -0400

    Fix plot titles.

--- R/plot.IRRsim.R
+++ R/plot.IRRsim.R
@@ -18,14 +18,16 @@ plot.IRRsim <- function(x,
 	test <- as.data.frame(x)
 	test$k <- as.character(test$k)
 	if(missing(stat)) {
-		tests.melted <- reshape2::melt(test, id.vars = c('nLevels', 'nEvents', 'k', 'simAgreement', 'agreement'))
+		tests.melted <- reshape2::melt(test, id.vars = c('nLevels', 'nEvents',
+				'k', 'simAgreement', 'agreement',
+				'skewness', 'kurtosis', 'MaxResponseDiff'))
 		p <- ggplot(tests.melted, aes(x = agreement, y = value, color = k)) +
 			geom_point(alpha = point.alpha) +
 			facet_wrap(~ variable) +
 			xlim(c(0,1)) +
 			xlab('Percent Rater Agreement') + ylab('ICC') +
-			ggtitle(paste0('Inter-Rater Reliability with ', test[1,]$nLevels, ' Scoring Levels and ',
-						   test[1,]$k, ' Raters'))
+			ggtitle(paste0('Inter-Rater Reliability with ', test[1,]$nLevels,
+						   ' Scoring Levels and ', test[1,]$k, ' Raters'))
 	} else {
 		if(!stat %in% names(test)) {
 			stop(paste0(stat, ' is not a valid IRR statistic.'))
