069b4a611aa54dcce4eec6dcf259c238e524d741
Jason Bryer <jason@bryer.org>
2019-03-18 17:57:38 -0400

    Added parameter to clean the cache. Also print messages regarding the status of the cache.

--- R/IRRsim_demo.R
+++ R/IRRsim_demo.R
@@ -1,8 +1,17 @@
 #' Run the Inter-Rater Reliability shiny app.
 #'
 #' @export
-IRRsim_demo <- function() {
+IRRsim_demo <- function(clean = FALSE) {
 	dir <- paste0(find.package('IRRsim'), '/shiny')
 	message(paste0("Running shiny app from ", dir))
+	cache.file <- paste0(dir, '/IRRsimShinyCache.rda')
+	if(file.exists(cache.file)) {
+		if(clean) {
+			message(paste0('Deleting old cache file.'))
+			unlink(paste0(dir, '/IRRsimShinyCache.rda'))
+		} else {
+			message('Cache file found. Set clean = TRUE to remove.')
+		}
+	}
 	shiny::runApp(appDir = dir)
 }
